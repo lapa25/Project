d08730b25384621c4f1559c690ca4d5bb4e124b5
alapa <lapantonina2505@mail.ru>
2021-11-23 20:25:52 +0300

    different colours + interface

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtCore import QPoint
 from random import randint
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtWidgets
 
 
 class Ui_MainWindow(object):
